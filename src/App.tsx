627f308edd8a71f4e94483b33501dc6daced6b6d
Joao <oliveira110965@gmail.com>
2026-04-07 19:46:31 -0300

    componetes tema e postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/footer/Footer"
-import NavBar from "./components/navbar/NavBar"
+
 import Home from "./pages/home/Home"
-import Login from "./pages/login/Login"
 import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
 import { AuthProvider } from "./contexts/AuthContext"
+import DeletarTema from "./components/tema/deletartema/DeletarTema"
+import FormTema from "./components/tema/formtema/FormTema"
+import ListaTemas from "./components/tema/listatemas/ListarTema"
+import Navbar from "./components/navbar/NavBar"
 
 
 
 function App() {
-
-
+  
   return (
     <>
       <AuthProvider>
         <BrowserRouter>
-          <NavBar />
+          <Navbar />
           <div className="min-h-[80vh]">
-
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+							<Route path="/cadastrartema" element={<FormTema />} />
+							<Route path="/editartema/:id" element={<FormTema />} />
+							<Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
-
           </div>
           <Footer />
         </BrowserRouter>
